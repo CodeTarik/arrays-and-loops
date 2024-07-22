18ff59739c69222e79bb25dc232b5a9adc3fb648
Tarik Bozüslü <boz.tarik38@gmail.com>
2024-07-22 23:18:20 +0200

    change arrays

--- arrays_and_loops.js
+++ arrays_and_loops.js
@@ -2,5 +2,9 @@
 let myList = [12, "Banana", true];
 
 function initArrays(){
-    console.log(myList[2]);
+    myList[0] = "no banana";
+    myList[1] = "hello";
+    myList[2] = "moin";
+
+    console.log(myList);
 }
