ce326577ec7bcdba8bfcc28d84a286408b5e14bb
Tarik Bozüslü <boz.tarik38@gmail.com>
2024-07-23 23:42:28 +0200

    first commit pop()

---
+++ pop_and_push.js
@@ -0,0 +1,9 @@
+let fruits = ["apple", "banana", "strawberry", "pineapple", "lime"]
+
+
+function initArrays(){
+fruits.pop();           // removes the last element in an array
+document.getElementById("sweet").innerHTML = fruits;
+fruits.pop();
+fruits.pop();
+}
