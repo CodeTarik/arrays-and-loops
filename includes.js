ef9ea208dd6bbc8250ebac3ff9c683906a9db541
Tarik Bozüslü <boz.tarik38@gmail.com>
2024-07-25 21:44:07 +0200

    includes()

---
+++ includes.js
@@ -0,0 +1,15 @@
+//let names = ['Anna', 'Ben', 'Clara']
+
+let fruits = ["apple", "banana", "strawberry", "pineapple", "lime"]
+
+let singleTestVar = "tarik";
+
+function containsElement(array, element){
+    return array.includes(element);
+
+}
+
+
+function initArrays(){
+    console.log(containsElement(fruits, "banana"));     //the length is changed and adds a new element at the end of the array
+    }
