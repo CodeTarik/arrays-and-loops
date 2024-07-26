8b7d5011e466c5fb0311bc76ecf6964c65392f52
Tarik Bozüslü <boz.tarik38@gmail.com>
2024-07-27 01:28:04 +0200

    third element

--- loop.js
+++ loop.js
@@ -42,3 +42,9 @@ function printNumbersReverse() {
     }
 }
 
+function printEveryThirdElement(){
+    let numb = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    for (let i = 0; i < numb.length; i += 3) {
+    console.log(numb[i]);
+    }
+}
