65ddf22519f581d3caf0ca341cfb32bb2ce8974d
Tarik Bozüslü <boz.tarik38@gmail.com>
2024-07-26 21:11:46 +0200

    for loop basic

---
+++ loop.js
@@ -0,0 +1,15 @@
+let myList = [12, "Banana", 1];
+let fruits = ["Banana", "Orange", "Apple", "Mango"];
+let singleTestVar = "asd";
+
+function containsElement(array, element){
+    return array.includes(element);
+}
+
+function initArrays(){
+    
+    for (let indexFruits = 0; indexFruits < 7; indexFruits += 3) {
+        console.log(indexFruits);   
+    }
+    
+}
